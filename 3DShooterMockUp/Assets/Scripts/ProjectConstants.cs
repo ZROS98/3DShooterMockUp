1a3cc48d166ee25f88a70f76e3d490bce03199f5
Vladyslav Riabchuk <vladyslav.riabchuk@4experience.co>
2023-04-15 00:47:36 +0200

    Add project constants

---
+++ 3DShooterMockUp/Assets/Scripts/ProjectConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace SUtilities.Utilities
+{
+    public class ProjectConstants
+    {
+        public const string ROOT_MENU_PATH = "Shooter Mock-up System/";
+        public const string SHOOTER_MOCK_UP_MENU_PATH = ROOT_MENU_PATH + "Shooter Mock-up Settings/";
+
+        public const string HEADER_REFERENCES = "References";
+        public const string HEADER_SETTINGS = "Settings";
+    }
+}
